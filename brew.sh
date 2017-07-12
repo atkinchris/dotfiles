50fc6e5c4f967cbb35118fcd10f0f5e8413a726f
Chris Atkin <atkinchris@gmail.com>
2017-07-12 17:46:50 +0100

    Add NVM and Yarn to Brew

--- brew.sh
+++ brew.sh
@@ -6,8 +6,12 @@ brew install coreutils
 brew install git git-lfs
 brew install wget
 brew install zsh zsh-completions
+brew install nvm yarn
 brew cleanup
 
 git lfs install
+
+mkdir ~/.nvm
+nvm install node
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
