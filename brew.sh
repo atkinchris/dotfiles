896659b9542741716274ecd8140757e20be3570c
Chris Atkin <atkinchris@gmail.com>
2017-07-12 17:38:09 +0100

    Remove tools that are no longer used or installed manually

--- brew.sh
+++ brew.sh
@@ -2,48 +2,12 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-brew update
-brew upgrade --all
-
-brew tap homebrew/versions
-
 brew install coreutils
-brew install git
-brew install git-lfs
-brew install nvm
+brew install git git-lfs
+brew install wget
 brew install zsh zsh-completions
-brew install wget --with-iri
-brew install p7zip
-brew install speedtest_cli
-brew install ansible
-
-brew install vim --override-system-vi
-
-brew cask install alfred
-brew cask install dropbox
-brew cask install google-chrome
-brew cask install vlc
-brew cask install iterm2
-brew cask install github-desktop
-brew cask install atom
-brew cask install flux
-brew cask install spotify
-brew cask install onyx
-brew cask install tunnelbear
-brew cask install steam
-brew cask install wunderlist
-brew cask install the-unarchiver
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install slack
-
 brew cleanup
 
-mkdir ~/.nvm
-nvm install node
-
-apm stars --user atkinchris --install
+git lfs install
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
