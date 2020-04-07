7050ca4c8924b1cac601ec44e763459db23a949e
Chris Atkin <atkinchris@gmail.com>
2020-04-07 18:54:44 +0100

    Add Debian installer

---
+++ debian.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  tree \
+  vim \
+  wget \
+  zsh
+
+git lfs install
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sh ./bootstrap.sh
