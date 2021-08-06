9d261456cca1e4b17cdc2a6751d73f455c56ea0f
Chris Atkin <atkinchris@gmail.com>
2021-08-06 17:02:28 +0100

    Comment the brew script

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -eu
 
+# Setup brew
 if ! [ -x "$(command -v brew 2>/dev/null)" ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# Update and upgrade brew, just in case
 brew update
 brew upgrade
 
+# Install basic tools
 brew install \
   coreutils \
   fzf \
@@ -21,6 +24,7 @@ brew install \
   wget \
   zsh zsh-completions
 
+# Tap and install fonts
 brew tap homebrew/cask-fonts
 brew install --cask \
   font-hack-nerd-font \
@@ -28,23 +32,31 @@ brew install --cask \
   font-menlo-for-powerline \
   font-meslo-for-powerline
 
+# Housekeeping for brew
 brew cleanup
 
+# Setup git lfs
+git lfs install
+
+# Setup editor font
 cd `mktemp -d`
 git clone https://github.com/kencrocken/FiraCodeiScript.git .
 cp *.ttf /Library/Fonts
 cd -
 
-
+# Setup iTerm2 to use a preferences file
 mkdir -p ~/.iterm2
 cp ./.iterm2/com.googlecode.iterm2.plist ~/.iterm2/
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
+# Setup terminal tools, like oh-my-zsh, powerlevel10k and nvm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
+# Install vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# rsync dotfiles
 sh ./bootstrap.sh
