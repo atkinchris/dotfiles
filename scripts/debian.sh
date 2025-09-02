d06aee1f11dfcf79f2a54b175a2a0a1885a20a86
Chris Atkin <github@chrisatkin.co.uk>
2025-09-02 11:19:35 +0100

    Remove the clone depth

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y \
   zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
 
